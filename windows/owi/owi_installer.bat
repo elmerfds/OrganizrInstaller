43df5659ced98fb8cc71095fca0fb97bf28fbe69
elmerfdz <elmer.fdz@outlook.com>
2017-12-11 23:15:40 +0000

    OWI: Under the hood minor changes and DL files cleanup

--- windows/owi/owi_installer.bat
+++ windows/owi/owi_installer.bat
@@ -8,30 +8,34 @@ ECHO	 \:\/:/  / \::/:/  / \::/\/__/
 ECHO	  \::/  /   \::/  /   \:\__\  
 ECHO	   \/__/     \/__/     \/__/  
 ECHO.
-ECHO    v0.6.7 Beta
+ECHO    v0.6.9 Beta
 ECHO.
 pause
 ECHO.
-cd %~dp0
+SET nginx_v=1.12.2
+SET php_v=7.2.0
+SET nssm_v=2.24-101
+SET vcr_v=2017
+CD %~dp0
 ECHO Where do you want to install Nginx? e.g 'c:\nginx'
-set /p nginx_loc=
+SET /p nginx_loc=
 ECHO.
-ECHO 1. Downloading Nginx
+ECHO 1. Downloading Nginx %nginx_v%
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://nginx.org/download/nginx-1.12.2.zip', 'nginx.zip')"
 powershell -Command "Invoke-WebRequest http://nginx.org/download/nginx-1.12.2.zip -OutFile nginx.zip"
 ECHO.    Done!
 
-ECHO 2. Downloading PHP
+ECHO 2. Downloading PHP   %php_v%
 powershell -Command "(New-Object Net.WebClient).DownloadFile('http://windows.php.net/downloads/releases/php-7.2.0-nts-Win32-VC15-x64.zip', 'php.zip')"
 powershell -Command "Invoke-WebRequest http://windows.php.net/downloads/releases/php-7.2.0-nts-Win32-VC15-x64.zip -OutFile php.zip"
 ECHO.    Done!
 
-ECHO 3. Downloading NSSM
+ECHO 3. Downloading NSSM  %nssm_v%
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://nssm.cc/ci/nssm-2.24-101-g897c7ad.zip', 'nssm.zip')"
 powershell -Command "Invoke-WebRequest https://nssm.cc/ci/nssm-2.24-101-g897c7ad.zip -OutFile nssm.zip"
 ECHO.    Done!
 
-ECHO 4. Downloading Visual C++ Redistributable for Visual Studio 2017
+ECHO 4. Downloading Visual C++ Redistributable for Visual Studio %vcr_v%
 powershell -Command "Invoke-WebRequest https://download.microsoft.com/download/3/b/f/3bf6e759-c555-4595-8973-86b7b4312927/vc_redist.x64.exe -OutFile vc_redist.x64.exe"
 ECHO.    Done!
 
@@ -51,15 +55,15 @@ ECHO.    Done!
 ECHO.
 ECHO Moving Nginx and PHP to destination
 ECHO.
-move %~dp0nginx-* nginx
-move %~dp0nginx %nginx_loc%
-move %~dp0nssm-* nssm
-move %~dp0php %nginx_loc%\php
+MOVE %~dp0nginx-* nginx
+MOVE %~dp0nginx %nginx_loc%
+MOVE %~dp0nssm-* nssm
+MOVE %~dp0php %nginx_loc%\php
 
 ECHO.
 ECHO Moving NSSM to destination
 ECHO.
-move %~dp0nssm\win64\nssm.exe C:\Windows\System32
+MOVE %~dp0nssm\win64\nssm.exe C:\Windows\System32
 
 
 ECHO.
@@ -72,10 +76,10 @@ ECHO In order to save and reload Nginx configuration, you need to run the NGINX 
 ECHO Username: %username%
 set /p pass=" Password: "
 ECHO.  
-nssm install nginx %nginx_loc%\nginx.exe
-nssm set nginx ObjectName %userdomain%\%username% %pass%
-nssm start nginx
-nssm restart nginx
+NSSM install NGINX %nginx_loc%\nginx.exe
+NSSM set NGINX ObjectName %userdomain%\%username% %pass%
+NSSM start NGINX
+NSSM restart NGINX
 
 
 ECHO.
@@ -83,45 +87,57 @@ ECHO Installing Visual C++ Redistributable for Visual Studio 2017 [PHP 7+ requir
 vc_redist.x64.exe /q /norestart
 ECHO.
 ECHO Creating PHP service
-nssm install php %nginx_loc%\php\php-cgi.exe
-nssm set php AppParameters -b 127.0.0.1:9000
-nssm set php ObjectName %userdomain%\%username% %pass%
-nssm start php
-nssm restart php
+NSSM install PHP %nginx_loc%\php\php-cgi.exe
+NSSM set PHP AppParameters -b 127.0.0.1:9000
+NSSM set PHP ObjectName %userdomain%\%username% %pass%
+NSSM start PHP
+NSSM restart PHP
 
 ECHO.
 ECHO Downloading Organizr Master
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/causefx/Organizr/archive/master.zip', 'master.zip')"
 powershell -Command "Invoke-WebRequest https://github.com/causefx/Organizr/archive/master.zip -OutFile master.zip"
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('master.zip', '.'); }"
-move %~dp0Organizr-master organizr
-del /s /q %~dp0master.zip
+MOVE %~dp0Organizr-master organizr
+DEL /s /q %~dp0master.zip
 xcopy /e /i /y /s organizr %nginx_loc%\html\organizr
-rmdir /s /q organizr
+RMDIR /s /q organizr
 
 ECHO.
 ECHO Updating Nginx and PHP config
-copy %~dp0config\nginx.conf %nginx_loc%\conf\nginx.conf
-cd %nginx_loc%
+ECHO.
+COPY %~dp0config\nginx.conf %nginx_loc%\conf\nginx.conf
+CD %nginx_loc%
 nginx -s reload
-cd %~dp0
-
-copy %~dp0config\php.ini %nginx_loc%\php\php.ini
-
-cd %nginx_loc%
+CD %~dp0
+COPY %~dp0config\php.ini %nginx_loc%\php\php.ini
+CD %nginx_loc%
 nginx -s reload
-cd %~dp0
-nssm restart php
-nssm restart nginx
-echo.
+CD %~dp0
+NSSM restart PHP
+NSSM restart NGINX
+ECHO.
 timeout /t 4 /nobreak
-set /p "=Nginx status : " <nul
-nssm status nginx
-set /p "=PHP   status : " <nul
-nssm status php
-echo.
-echo Installation Completed
-echo.
-set /p "=To open Organizr [http://localhost] " <nul
+SET /p "=Nginx status : " <nul
+NSSM status NGINX
+SET /p "=PHP   status : " <nul
+NSSM status PHP
+ECHO.
+ECHO Installation Completed
+ECHO.
+SET /p "=To open Organizr [http://localhost] " <nul
 pause
-start http://localhost
+START http://localhost
+ECHO.
+ECHO Cleaning up downloaded Files
+ECHO.
+DEL /s /q %~dp0nginx.zip
+DEL /s /q %~dp0php.zip
+DEL /s /q %~dp0nssm.zip
+DEL /s /q %~dp0vc_redist.x64.exe
+RMDIR /s /q nssm
+ECHO.
+ECHO Done!
+ECHO.
+SET /p "=Exit: " <nul
+pause
