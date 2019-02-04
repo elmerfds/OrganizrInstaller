5f15970e647cf2afcbc21dfcf856c239396475ed
elmerfdz <elmerfdz@users.noreply.github.com>
2019-02-04 20:20:18 +0000

    super_exp

--- centos/oci/oc_installer.sh
+++ centos/oci/oc_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 #Organizr CentOS Installer
 #author: elmerfdz
-version=v1.0.0-1
+version=v1.2.0-0-super_exp
 #Org Requirements
 orgreqname=('Unzip' 'NGINX' 'PHP' 'PHP-ZIP' 'PDO:SQLite' 'PHP cURL' 'PHP simpleXML')
 orgreq=('unzip' 'nginx' 'php-fpm' 'php-zip' 'php-sqlite3' 'php-curl' 'php-xml')
@@ -17,6 +17,8 @@ SED=`which sed`
 CURRENT_DIR=`dirname $0`
 tmp='/tmp/Organizr'
 dlvar=0
+cred_folder='/etc/letsencrypt/.secrets/certbot'
+LE_WEB='/var/www/letsencrypt/.well-known/acme-challenge'
 
 #Modules
 #Organizr Requirement Module
@@ -173,26 +175,204 @@ CFvhostcreate_mod()
 
 LEvhostcreate_mod()        
        {
-		systemctl start nginx
-		if [ "$org_v" == "1" ] && [ "$vhost_template" == "LE" ]
+		echo
+		if [ "$vhost_template" == "LE" ] || [ "$vhost_template" == "le" ]; 
 		then
-		cp $CURRENT_DIR/templates/orgv1_le.template $CONFIG
-		cp -a $CURRENT_DIR/config/le/. $NGINX_LOC/config
+			echo -e "\e[1;36m> Do you want to generate Let's Encrypt SSL certs as well? [y/n].\e[0m"
+			printf '\e[1;36m- \e[0m'
+			read -r LEcert_create
+			LEcert_create=${LEcert_create:-Y}
+			if [ "$LEcert_create" == "Y" ] || [ "$LEcert_create" == "y" ];
+			then		
+				echo -e "\e[1;36m> Please note, since you've selected the Let's Encrypt Option, will start by preparing your system to generte LE SSL certs.\e[0m" 
+				echo -e "\e[1;36m> Please make sure, you've configured your domain with the correct DNS records.\e[0m"
+				echo -e "\e[1;36m> If you're using CloudFlare (CF) as your DNS, then this is supported by this option.\e[0m"
+				echo -e "\e[1;36m> If you haven't preparared your setup to carry out the above, then please terminate this script.\e[0m"
+				echo 
+				echo -e "\e[1;36m> Or press any key to continue...\e[0m"		 
+				read 
+				echo 
+				echo -e "\e[1;36m> LE Cert type?:\e[0m"
+				echo
+				echo -e "\e[1;36m[S] \e[0mSingle Domain Cert"
+				echo -e "\e[1;36m[W] \e[0mWildcard"
+				echo
+				printf '\e[1;36m- \e[0m'
+				read -r LEcert_type
+				LEcert_type=${LEcert_type:-W}
+			
 
-		elif [ "$org_v" == "2" ] && [ "$vhost_template" == "LE" ]
-		then
-		cp $CURRENT_DIR/templates/orgv2_le.template $CONFIG
-		cp -a $CURRENT_DIR/config/le/. $NGINX_LOC/config
+				if [ "$LEcert_type" == "W" ] || [ "$LEcert_type" == "w" ];
+				then
+					echo
+					echo -e "\e[1;36m> Is your domain on Cloudflare? [y/n] .\e[0m"
+					echo  "- Going ahead with the above will automate the DNS / dns-01 challenges for you."
+					echo  "- To do that, python3-pip & certbot-dns-cloudflare pip3 package wll be installed"
+					printf '\e[1;36m- [y/n]: \e[0m'
+					read -r dns_plugin
+					dns_plugin=${dns_plugin:-n}
+					echo		
+				fi
+			fi	
 		
+			mkdir -p $NGINX_APPS 								#Apps folder
+			mkdir -p $NGINX_CONFIG/$DOMAIN
+			cp -a $CURRENT_DIR/config/apps/. $NGINX_APPS  		#Apps conf files
+			cp -a $CURRENT_DIR/config/le/. $NGINX_CONFIG/$DOMAIN 	#LE conf file
 
-		mv $NGINX_LOC/config/domain.com.conf $NGINX_LOC/config/$DOMAIN.conf
-		mv $NGINX_LOC/config/domain.com_ssl.conf $NGINX_LOC/config/${DOMAIN}_ssl.conf
-		CONFIG_DOMAIN=$NGINX_CONFIG/$DOMAIN.conf
-		mkdir -p $NGINX_CONFIG/ssl/$DOMAIN
-		chmod -R 755 $NGINX_CONFIG/ssl/$DOMAIN
-		fi
+			if [ "$org_v" == "1" ] && [ "$vhost_template" == "LE" ] || [ "$vhost_template" == "le" ]
+			then
+				LEcertbot_mod
+				if [ "$LEcert_create" == "Y" ] || [ "$LEcert_create" == "y" ]
+				then
+					cp $CURRENT_DIR/templates/le/orgv1_le.template $CONFIG
+					if [ "$LEcert_type" == "W" ] || [ "$LEcert_type" == "w" ]
+					then
+						subd='www'
+						subd_doma="$DOMAIN" 
+						serv_name="$subd.$DOMAIN $DOMAIN"  		
+			
+					elif [ "$LEcert_type" == "S" ] || [ "$LEcert_type" == "s" ]
+					then
+						subd_doma="$DOMAIN"
+						serv_name="$DOMAIN"   
+						#Create LE Certbot renewal cron job
+					fi
 
-	}
+				elif [ "$LEcert_create" == "N" ] || [ "$LEcert_create" == "n" ]
+				then
+						cp $CURRENT_DIR/templates/le/orgv1_le_no_ssl.template $CONFIG
+						subd_doma="$DOMAIN"
+						serv_name="$DOMAIN"   
+				fi	
+					
+			elif [ "$org_v" == "2" ] && [ "$vhost_template" == "LE" ] || [ "$vhost_template" == "le" ]
+			then
+				LEcertbot_mod
+				if [ "$LEcert_create" == "Y" ] || [ "$LEcert_create" == "y" ]
+				then
+					cp $CURRENT_DIR/templates/le/orgv2_le.template $CONFIG				
+					if [ "$LEcert_type" == "W" ] || [ "$LEcert_type" == "w" ]
+					then
+						subd='www'
+						subd_doma="$DOMAIN" 
+						serv_name="$subd.$DOMAIN $DOMAIN"  				
+							
+					elif [ "$LEcert_type" == "S" ] || [ "$LEcert_type" == "s" ]
+					then
+						subd_doma="$DOMAIN"
+						serv_name="$DOMAIN" 
+					fi
+
+				elif [ "$LEcert_create" == "N" ] || [ "$LEcert_create" == "n" ]
+				then
+						cp $CURRENT_DIR/templates/le/orgv2_le_no_ssl.template $CONFIG
+						subd_doma="$DOMAIN"
+						serv_name="$DOMAIN"   				
+				fi		
+			fi
+		fi	
+		}
+
+LEcertbot_mod() 
+		{
+			if [ ! -d "$LE_WEB" ]; then
+			mkdir -p $LE_WEB
+			fi
+			
+			#Configuring permissions on LE web folder
+			chmod -R 775 $LE_WEB
+			chown -R nginx:$(logname) $LE_WEB
+
+			#Copy LE TEMP conf file so that LE can connect to server and continue to generate the certs
+			cp $CURRENT_DIR/templates/le/le_temp.template $CONFIG
+			$SED -i "s/DOMAIN/$DOMAIN/g" $CONFIG
+			
+			#Delete default.conf nginx site
+			mkdir -p $tmp/bk/nginx_default_site
+ 			if [ -e $NGINX_SITES/default ] 
+			then cp -a $NGINX_SITES/default $tmp/bk/nginx_default_site
+			fi			
+			rm -r -f $NGINX_SITES/default
+			rm -r -f $NGINX_SITES_ENABLED/default
+
+			# create symlink to enable site
+			ln -s $CONFIG $NGINX_SITES_ENABLED/$DOMAIN.conf
+
+			if [ "$LEcert_create" == "Y" ] || [ "$LEcert_create" == "y" ];
+			then 
+				# reload Nginx to pull in new config
+				nginx -s reload
+				##Install certbot packages
+				yum -y install yum-utils
+				yum-config-manager --enable rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
+				yum install python2-certbot-nginx
+			fi
+
+			if [ "$dns_plugin" == "Y" ] || [ "$dns_plugin" == "y" ]
+			then
+				echo
+				echo -e "\e[1;36m> Enter your Cloudflare email.\e[0m"
+				printf '\e[1;36m- \e[0m' 
+				read -r CF_EMAIL
+				echo
+				echo -e "\e[1;36m> Enter your Cloudflare API.\e[0m" 
+				echo "- You can get your Cloudflare API from here: https://dash.cloudflare.com/profile"
+				printf '\e[1;36m- \e[0m' 
+				read -r CF_API
+				echo
+				apt-get install certbot python3-pip -y
+				pip3 install certbot-dns-cloudflare
+
+			mkdir -p $cred_folder #create secret folder to store Certbot CF plugin creds
+			cp -a $CURRENT_DIR/config/le-dnsplugins/cf/. $cred_folder #copy CF credentials file
+			#Update CF plugin file
+			$SED -i "s/CF_EMAIL/$CF_EMAIL/g" $cred_folder/cloudflare.ini
+			$SED -i "s/CF_API/$CF_API/g" $cred_folder/cloudflare.ini
+			chmod -R 600 $cred_folder #debug
+
+			elif [ "$dns_plugin" == "N" ] || [ "$dns_plugin" == "n" ]
+			then
+				apt-get install certbot -y
+			fi
+
+			if [ "$LEcert_create" == "Y" ] || [ "$LEcert_create" == "y" ];
+			then
+				## Get wildcard certificate, Let's Encrypt
+				echo
+				echo -e "\e[1;36m> Enter an email address, which will be used to generate the SSL certs?.\e[0m"
+				if [ "$dns_plugin" == "Y" ] || [ "$dns_plugin" == "y" ];
+				then
+					echo -e "- Press Enter to use \e[1;36m$CF_EMAIL\e[0m or enter a different one"
+				fi
+				read -r email_var
+				email_var=${email_var:-$CF_EMAIL}
+			fi	
+
+			if [ "$LEcert_type" == "W" ] || [ "$LEcert_type" == "w" ]
+			then
+				if [ "$dns_plugin" == "Y" ] || [ "$dns_plugin" == "y" ]
+				then
+					certbot certonly --dns-cloudflare --dns-cloudflare-credentials $cred_folder/cloudflare.ini --server https://acme-v02.api.letsencrypt.org/directory --email $email_var --agree-tos --no-eff-email -d *.$DOMAIN -d $DOMAIN
+					#Adding wildcar cert auto renewal using CF DNS plugin, untested, let me know if anyone does.
+					{ crontab -l 2>/dev/null; echo "20 3 * * * certbot renew --noninteractive --dns-cloudflare --renew-hook "'"/etc/init.d/nginx reload"'""; } | crontab -
+				
+				elif [ "$dns_plugin" == "N" ] || [ "$dns_plugin" == "n" ]
+				then
+					certbot certonly --agree-tos --no-eff-email --email $email_var --server https://acme-v02.api.letsencrypt.org/directory --manual -d *.$DOMAIN -d $DOMAIN
+				fi
+			
+			elif [ "$LEcert_type" == "S" ] || [ "$LEcert_type" == "s" ]
+			then
+				certbot certonly --webroot --agree-tos --no-eff-email --email $email_var -w /var/www/letsencrypt -d $DOMAIN -d $DOMAIN
+				#Create LE Certbot renewal cron job
+				{ crontab -l 2>/dev/null; echo "20 3 * * * certbot renew --noninteractive --renew-hook "'"/etc/init.d/nginx reload"'""; } | crontab -
+			fi
+
+			## Once Cert has been generated, delete the created conf file.
+			rm -r -f $NGINX_SITES/$DOMAIN.conf
+			rm -r -f $NGINX_SITES_ENABLED/$DOMAIN.conf
+		}		
 
 #Organizr download module
 orgdl_mod()
